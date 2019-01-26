fd2093e421922ea84235e0048e4d5d250559ab5e
1130034920@qq.com <1130034920@qq.com>
2019-01-27 00:44:41 +0800

    6-9 如何使用 React-redux（2）

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,16 +3,22 @@ import { connect } from "react-redux";
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    // this.state = store.getState();
     this.state = {};
   }
-  handleChange() {}
+  handleInputChange(e) {
+    console.log(e.target.value);
+  }
+  handleClick() {}
   render() {
     return (
       <div>
         <div>
-          <input placeholder="xxxx" onChange={this.handleChange} />
-          <button>提交</button>
+          <input
+            value={this.props.inputValue}
+            placeholder="xxxx"
+            onChange={this.props.changeInputValue}
+          />
+          <button onClick={this.handleClick.bind(this)}>提交</button>
         </div>
         <ul>
           <li>item</li>
@@ -23,7 +29,21 @@ class TodoList extends Component {
 }
 const mapStateToProps = state => {
   return {
-    state: state
+    inputValue: state.inputValue,
+    list: state.list
   };
 };
-export default connect(mapStateToProps)(TodoList);
+// store.dispatch
+const mapDispatchToProps = dispatch => {
+  return {
+    changeInputValue(e) {
+      const action = { type: "change_input_value", value: e.target.value };
+      console.log(e.target.value);
+      dispatch(action)
+    }
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
