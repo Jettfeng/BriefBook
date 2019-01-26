44568035428a58e1553b4a8dd8a5c0aa2171dd27
1130034920@qq.com <1130034920@qq.com>
2019-01-26 15:36:10 +0800

    6-1 UI组件和容器组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
-import { Input, Button, List } from "antd";
 import store from "./store";
-import { getInputChangeAction,getAddItemAction ,getDeleteItemAction} from "./store/actionCreators";
+import {
+  getInputChangeAction,
+  getAddItemAction,
+  getDeleteItemAction
+} from "./store/actionCreators";
+import TodoListUI from "./TodoListUI";
 
 class TodoList extends Component {
   constructor(props) {
@@ -11,6 +15,7 @@ class TodoList extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleStoreChange = this.handleStoreChange.bind(this);
     this.handleBtnClick = this.handleBtnClick.bind(this);
+    // this.handleItemDelete = this.handleItemDelete.bind(this);
     store.subscribe(this.handleStoreChange);
   }
   handleInputChange(e) {
@@ -25,34 +30,19 @@ class TodoList extends Component {
     store.dispatch(action);
   }
   handleItemDelete(index) {
+    console.log(index);
     const action = getDeleteItemAction(index);
     store.dispatch(action);
   }
   render() {
     return (
-      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
-        <div>
-          <Input
-            value={this.state.inputValue}
-            placeholder="todo info"
-            style={{ width: "300px", marginRight: "10px" }}
-            onChange={this.handleInputChange}
-          />
-          <Button type="primary" onClick={this.handleBtnClick}>
-            提交
-          </Button>
-        </div>
-        <List
-          style={{ marginTop: "10px", width: "300px" }}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        list={this.state.list}
+        handleInputChange={this.handleInputChange}
+        handleBtnClick={this.handleBtnClick}
+        handleItemDelete={this.handleItemDelete}
+      />
     );
   }
 }
