2f93e436a42c3cbffd1fc908b7f2c1fa61704c87
1130034920@qq.com <1130034920@qq.com>
2019-01-27 12:38:34 +0800

    7-1 项目目录搭建，Styled-Components 与 Reset.css 的结合使用

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component,Fragment } from "react";
+import { GlobalStyle } from "./style.js";
+import axios from "axios";
 
 class App extends Component {
+  componentDidMount() {
+    axios.get("/list").then(res => {
+      console.log(res);
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <GlobalStyle />
+        <div className="dell">hello word</div>
+      </Fragment>
     );
   }
 }
