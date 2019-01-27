c4d325a1045ccd652b832cda01870187aa91f1fa
1130034920@qq.com <1130034920@qq.com>
2019-01-27 15:12:44 +0800

    7-2 使用 styled-components 完成 Header 组件布局（1）

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Component,Fragment } from "react";
+import React, { Component, Fragment } from "react";
+import Header from "./common/header";
+// -------------------------------------------------------
 import { GlobalStyle } from "./style.js";
 import axios from "axios";
-
+// -----------------------------------------------------------
 class App extends Component {
-  componentDidMount() {
-    axios.get("/list").then(res => {
-      console.log(res);
-    });
-  }
   render() {
     return (
       <Fragment>
         <GlobalStyle />
+        <Header />
         <div className="dell">hello word</div>
       </Fragment>
     );
