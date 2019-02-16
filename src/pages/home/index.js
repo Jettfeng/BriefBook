ccd4c8cd45fabc79e455ab91dbb654927f8dc9ec
1130034920@qq.com <1130034920@qq.com>
2019-02-16 11:23:54 +0800

    8-9 返回顶部功能实现

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,11 +5,21 @@ import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Writer from "./components/Writer";
 import { actionCreators } from "./store";
-import { HomeWrapper, HomeLeft, HomeRight } from "./style";
+import { HomeWrapper, HomeLeft, HomeRight, BackTop } from "./style";
 
 class Home extends Component {
   componentDidMount() {
     this.props.changeHomeData();
+    this.bindEvents();
+  }
+  bindEvents() {
+    window.addEventListener("scroll", this.props.changeScrollTopShow);
+  }
+  handleScrollTop() {
+    window.scrollTo(0, 0);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.props.changeScrollTopShow);
   }
   render() {
     return (
@@ -27,27 +37,30 @@ class Home extends Component {
           <Recommend />
           <Writer />
         </HomeRight>
+        {this.props.showScroll ? (
+          <BackTop onClick={this.handleScrollTop}>顶部</BackTop>
+        ) : null}
       </HomeWrapper>
     );
   }
 }
+const mapState = state => ({
+  showScroll: state.getIn(["home", "showScroll"])
+});
 const mapDispatch = dispatch => ({
   changeHomeData() {
     const action = actionCreators.getHomeInfo();
     dispatch(action);
-    // axios.get("/api/home.json").then(res => {
-    //   const result = res.data.data;
-    //   const action = {
-    //     type: "change_home_data",
-    //     topicList: result.topicList,
-    //     articleList: result.articleList,
-    //     recommendList: result.recommendList
-    //   };
-    //   dispatch(action);
-    // });
+  },
+  changeScrollTopShow() {
+    if (document.documentElement.scrollTop > 100) {
+      dispatch(actionCreators.toggleTopShow(true));
+    } else {
+      dispatch(actionCreators.toggleTopShow(false));
+    }
   }
 });
 export default connect(
-  null,
+  mapState,
   mapDispatch
 )(Home);
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -6,8 +6,10 @@ const defaultState = fromJS({
   topicList: [],
   articleList: [],
   recommendList: [],
-  articlePage: 1
+  articlePage: 1,
+  showScroll: false
 });
+
 const changeHomeData = (state, action) => {
   return state.merge({
     topicList: fromJS(action.topicList),
@@ -15,12 +17,14 @@ const changeHomeData = (state, action) => {
     recommendList: fromJS(action.recommendList)
   });
 };
+
 const addArticleList = (state, action) => {
   return state.merge({
     articleList: state.get("articleList").concat(action.list),
     articlePage: action.nextPage
   });
 };
+
 export default (state = defaultState, action) => {
   switch (action.type) {
     case constants.CHANGE_HOME_DATA:
@@ -28,6 +32,8 @@ export default (state = defaultState, action) => {
       return changeHomeData(state, action);
     case constants.ADD_ARTICLE_LIST:
       return addArticleList(state, action);
+    case constants.TOGGLE_SCROLL_TOP:
+      return state.set("showScroll", action.show);
     default:
       return state;
   }
