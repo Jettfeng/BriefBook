1a30409119ae1bb581336a278fd23b0119de727c
1130034920@qq.com <1130034920@qq.com>
2019-02-16 16:50:46 +0800

    9-2 使用redux管理详情页面数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
-import { DetailWrapper, Header,Content } from "./style";
+import { connect } from "react-redux";
+import { DetailWrapper, Header, Content } from "./style";
 class Detail extends Component {
+  componentDidMount() {}
   render() {
     return (
       <DetailWrapper>
-        <Header>HeaderHeaderHeaderHeader</Header>
-        <Content>
-          <img src="//upload-images.jianshu.io/upload_images/8106851-1f084c4fd5b7ca74.png"/>
-          <p>蜜饯的口感酸甜而且色泽艳丽，但是它是一种腌制食品，含有大量的热量和糖分，还含有许多致癌物质，在肠胃消化的时候会消耗自身体内的营养元素，因此会影响孩子的正常生长发育。</p>
-          <p>蜜饯的口感酸甜而且色泽艳丽，但是它是一种腌制食品，含有大量的热量和糖分，还含有许多致癌物质，在肠胃消化的时候会消耗自身体内的营养元素，因此会影响孩子的正常生长发育。</p>
-          <p>蜜饯的口感酸甜而且色泽艳丽，但是它是一种腌制食品，含有大量的热量和糖分，还含有许多致癌物质，在肠胃消化的时候会消耗自身体内的营养元素，因此会影响孩子的正常生长发育。</p>
-        </Content>
+        <Header>{this.props.title}</Header>
+        <Content dangerouslySetInnerHTML={{ __html: this.props.content }} />
       </DetailWrapper>
     );
   }
 }
-export default Detail;
+const mapState = state => ({
+  title: state.getIn(["detail", "title"]),
+  content: state.getIn(["detail", "content"])
+});
+export default connect(
+  mapState,
+  null
+)(Detail);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,10 +2,12 @@
 import { combineReducers } from "redux-immutable";
 import { reducer as headerReducer } from "../common/header/store";
 import { reducer as HomeReducer } from "../pages/home/store";
+import { reducer as detailReducer } from "../pages/detail/store";
 // redux-immutable
 const reducer = combineReducers({
   header: headerReducer,
-  home: HomeReducer
+  home: HomeReducer,
+  detail: detailReducer
 });
 
 export default reducer;
