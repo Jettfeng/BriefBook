69376ef0d7a00cba1b86aa9b1d60ad1f01b7cd31
1130034920@qq.com <1130034920@qq.com>
2019-02-16 11:58:13 +0800

    9-1 详情页面布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import { DetailWrapper, Header,Content } from "./style";
 class Detail extends Component {
   render() {
-    return <div>Detail</div>;
+    return (
+      <DetailWrapper>
+        <Header>HeaderHeaderHeaderHeader</Header>
+        <Content>
+          <img src="//upload-images.jianshu.io/upload_images/8106851-1f084c4fd5b7ca74.png"/>
+          <p>蜜饯的口感酸甜而且色泽艳丽，但是它是一种腌制食品，含有大量的热量和糖分，还含有许多致癌物质，在肠胃消化的时候会消耗自身体内的营养元素，因此会影响孩子的正常生长发育。</p>
+          <p>蜜饯的口感酸甜而且色泽艳丽，但是它是一种腌制食品，含有大量的热量和糖分，还含有许多致癌物质，在肠胃消化的时候会消耗自身体内的营养元素，因此会影响孩子的正常生长发育。</p>
+          <p>蜜饯的口感酸甜而且色泽艳丽，但是它是一种腌制食品，含有大量的热量和糖分，还含有许多致癌物质，在肠胃消化的时候会消耗自身体内的营养元素，因此会影响孩子的正常生长发育。</p>
+        </Content>
+      </DetailWrapper>
+    );
   }
 }
 export default Detail;
